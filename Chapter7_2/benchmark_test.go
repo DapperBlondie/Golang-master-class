1ef05ea905091a8a5fa4d1489d9cb0927cd1950f
DapperBlondie <alirezagharib110@gmail.com>
2021-05-15 00:33:22 +0430

    BenchMark function created and tested

---
+++ Chapter7_2/benchmark_test.go
@@ -0,0 +1,39 @@
+package Chapter7_2
+
+import "testing"
+
+var RESULT int
+
+func BenchmarkFibo1(b *testing.B) {
+
+	var r int
+	for i := 0; i < b.N; i++ {
+
+		r = fibo1(50)
+	}
+
+	RESULT = r
+}
+
+func BenchmarkFibo2(b *testing.B) {
+
+	var r int
+	for i := 0; i < b.N; i++ {
+
+		r = fibo2(50)
+	}
+
+	RESULT = r
+}
+
+func BenchmarkFibo3(b *testing.B) {
+
+	var r int
+	for i := 0; i < b.N; i++ {
+
+		r = fibo3(50)
+	}
+
+	RESULT = r
+	//Output:131342
+}
